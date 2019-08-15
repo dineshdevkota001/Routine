9dcb0e5103c8a206c4559f6cc5b9bf5047ef5b02
dineshdevkota001 <dineshdevkota001@gmail.com>
2019-08-15 16:49:02 +0545

    added node modules

--- frontend/src/Period.jsx
+++ frontend/src/Period.jsx
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 export default class Period extends Component {
     render() {
         return (
-            <div>
-                
+            <div id = "name">
+                <select>
+                    <option id = "i"></option>
+                    <option id = "j"></option>
+                    <option id = "k"></option>
+                </select>
             </div>
         );
     }
