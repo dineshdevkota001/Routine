c155b45adc8edd302573440875de830e964540b0
dinesh001 <dineshdevkota001@gmail.com>
2019-08-16 11:54:33 +0545

    created reactas an app and indpendent from backend

--- frontend/src/Period.jsx
+++ frontend/src/Period.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 export default class Period extends Component {
+    getToptions(){
+        return(
+            <div>
+            <option value="option1">option1</option>
+            <option value="option2">option2</option>
+            <option value="option3">option3</option>
+            <option value="option3">option3</option>
+            <option value="option4">option4</option>
+            </div>
+        );
+    }
+    getTeacher(){
+        return (
+            <div id = "teacherName">
+                <select name="teacher" id="teacherid">
+                    <input type="text"/>
+                    {this.getToptions()}
+                </select>
+            </div>
+        );
+    }
+
+
     render() {
         return (
-            <div id = "name">
-                <select>
-                    <option id = "i"></option>
-                    <option id = "j"></option>
-                    <option id = "k"></option>
-                </select>
+            <div>
+                <p>my name is diensh</p>
+                {getTeacher()}
             </div>
         );
     }
